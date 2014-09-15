ac0fcbc6e2371f8a82597ed357f181ad538d7796
Saleh Hamadeh <salehhamadeh@gmail.com>
2014-09-15 08:53:49 -0400

    Make blog more discoverable by RSS readers.

--- blog-source/buildblog.sh
+++ blog-source/buildblog.sh
@@ -3,4 +3,5 @@ jekyll build
 rm -rf ../blog
 mv _site/index.html ../blog.html
 mv _site/blog ../blog
-mv _site/feed.xml ../blog/feed.xml
+cp _site/feed.xml ../blog/feed.xml
+mv _site/feed.xml ../feed.xml
